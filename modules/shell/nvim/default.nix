63b0816482724f196b3d33372864be1979ec66b7
Mike Medford <mike@medford.dev>
2024-10-17 02:43:40 -0500

    move neovim config

--- modules/shell/nvim/default.nix
+++ modules/shell/nvim/default.nix
@@ -2,24 +2,8 @@
 
 {
   home-manager.users.${config.user} = {
-    xdg.configFile.nvimAfter = {
-      source = ./after;
-      target = "./nvim/after";
-    };
-
-    xdg.configFile.nvimLua = {
-      source = ./lua;
-      target = "./nvim/lua";
-    };
-
-    xdg.configFile.nvimSpell = {
-      source = ./spell;
-      target = "./nvim/spell";
-    };
-
-    xdg.configFile.nvimInit = {
-      source = ./init.lua;
-      target = "./nvim/init.lua";
+    xdg.configFile.nvim = {
+      source = ../../../config/nvim;
     };
   };
 }
