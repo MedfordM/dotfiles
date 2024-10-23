bf50eb9438c0feec62aac3f571da2593998838ad
Mike Medford <mike@medford.dev>
2024-10-23 12:54:26 -0500

    use initExtraFirst

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -15,20 +15,20 @@
           expireDuplicatesFirst = true;
           ignoreDups = true;
         };
-        initExtra = ''
+        initExtraFirst = ''
           autoload -U up-line-or-beginning-search
           autoload -U down-line-or-beginning-search
           zle -N up-line-or-beginning-search
           zle -N down-line-or-beginning-search
-
+          zstyle ':completion:*:git-checkout:*' sort false
+          zstyle ':completion:*:descriptions' format '[%d]'
+        '';
+        initExtra = ''
           bindkey -e "\e[H" beginning-of-line
           bindkey -e "\e[F" end-of-line
           bindkey -e "\e[A" up-line-or-beginning-search
           bindkey -e "\e[B" down-line-or-beginning-search
-
-          zstyle ':completion:*:git-checkout:*' sort false
-          zstyle ':completion:*:descriptions' format '[%d]'
-          '';
+        '';
       };
     };
   };
