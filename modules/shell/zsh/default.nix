f7fd4c560c6d8b6342ed5d398db5815516ed46c0
Mike Medford <mike@medford.dev>
2024-10-25 09:14:34 -0500

    history search on up/down keys

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -7,6 +7,7 @@
         enable = true;
         dotDir = ".config/zsh";
         enableCompletion = true;
+        defaultKeymap = "viins";
         history = {
           path = "$ZDOTDIR/.zsh_history";
           share = true;
@@ -16,18 +17,21 @@
           ignoreDups = true;
         };
         initExtraFirst = ''
-          autoload -U up-line-or-beginning-search
-          autoload -U down-line-or-beginning-search
+          autoload up-line-or-beginning-search
+          autoload down-line-or-beginning-search
           zle -N up-line-or-beginning-search
           zle -N down-line-or-beginning-search
           zstyle ':completion:*:git-checkout:*' sort false
           zstyle ':completion:*:descriptions' format '[%d]'
         '';
         initExtra = ''
-          bindkey -e "\e[H" beginning-of-line
-          bindkey -e "\e[F" end-of-line
-          bindkey -e "\e[A" up-line-or-beginning-search
-          bindkey -e "\e[B" down-line-or-beginning-search
+          bindkey -M viins "\\e[H" beginning-of-line
+          bindkey -M viins "\\e[F" end-of-line
+          bindkey -M viins "\\e[A" up-line-or-beginning-search
+          bindkey -M viins "\\e[B" down-line-or-beginning-search
+          bindkey -M viins "\\eOA" up-line-or-beginning-search
+          bindkey -M viins "\\eOB" down-line-or-beginning-search
+          bindkey -M vicmd "\\e[110;;110u" down
         '';
       };
     };
