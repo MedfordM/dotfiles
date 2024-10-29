e22cca3549320afa98caa8c65853270a22c09fd6
Mike Medford <mike@medford.dev>
2024-10-29 11:32:46 -0500

    remove hyprlock

--- modules/services/windowManagers/hyprland/default.nix
+++ modules/services/windowManagers/hyprland/default.nix
@@ -20,25 +20,13 @@
         waybar
         wofi
         hypridle
-        hyprlock
       ];
       services.hypridle = {
         enable = true;
         settings = {
-          general = {
-            lock_cmd = "pidof hyprlock || hyprlock";
-            before_sleep_cmd = "loginctl lock-session";
-            after_sleep_cmd = "hyprctl dispatch dpms on";
-          };
-
           listener = [
             {
-              timeout = 15;
-              on-timeout = "brightnessctl -s set 10";
-              on-resume = "brightnessctl -r";
-            }
-            {
-              timeout = 330;
+              timeout = 120;
               on-timeout = "hyprctl dispatch dpms off";
               on-resume = "hyprctl dispatch dpms on";
             }
