dc2c3674b9b119f39488d193fe514a9ca98dd0e5
Mike Medford <mike@medford.dev>
2024-10-29 10:52:20 -0500

    add hyprlock and hypridle

--- modules/services/windowManagers/hyprland/default.nix
+++ modules/services/windowManagers/hyprland/default.nix
@@ -19,7 +19,36 @@
       home.packages = with pkgs; [
         waybar
         wofi
+        hypridle
+        hyprlock
       ];
+      services.hypridle = {
+        enable = true;
+        settings = {
+          general = {
+            lock_cmd = "pidof hyprlock || hyprlock";
+            before_sleep_cmd = "loginctl lock-session";
+            after_sleep_cmd = "hyprctl dispatch dpms on";
+          };
+
+          listener = [
+            {
+              timeout = 15;
+              on-timeout = "brightnessctl -s set 10";
+              on-resume = "brightnessctl -r";
+            }
+            {
+              timeout = 330;
+              on-timeout = "hyprctl dispatch dpms off";
+              on-resume = "hyprctl dispatch dpms on";
+            }
+            {
+              timeout = 1800;
+              on-timeout = "systemctl suspend";
+            }
+          ];
+        };
+      };
     };
   };
 }
