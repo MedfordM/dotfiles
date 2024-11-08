24081623a41433c80c929dfbdd64cb4484f4150b
Mike Medford <mike@medford.dev>
2024-11-08 03:15:27 -0600

    add gke auth component

--- modules/shell/gcloud/default.nix
+++ modules/shell/gcloud/default.nix
@@ -1,9 +1,14 @@
 { config, pkgs, ... }:
 
+let
+  gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+    gke-gcloud-auth-plugin
+  ]);
+in
 {
   home-manager.users.${config.user} = {
     home.packages = with pkgs; [
-      google-cloud-sdk
+      gdk
       google-cloud-sql-proxy
     ];
   };
