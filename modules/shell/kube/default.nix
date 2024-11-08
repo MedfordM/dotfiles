05516363653b550f93382ce934c8718c67f83447
Mike Medford <mike@medford.dev>
2024-11-08 03:30:06 -0600

    add kubernetes

--- modules/shell/kube/default.nix
+++ modules/shell/kube/default.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.minikube
+  environment.systemPackages = with pkgs; [
+    kompose
+    kubectl
+    kubernetes
+    minikube
   ];
+
+  services.kubernetes = {
+    addons.dns.enable = true;
+    kubelet.extraOpts = "--fail-swap-on=false";
+  };
 }
