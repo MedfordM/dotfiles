a67c730eccec6b08b26bf62d87ee865217b22a7b
Mike Medford <mike@medford.dev>
2025-01-23 00:40:17 -0600

    remove unused plugins

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,67 +1,67 @@
 return {
-  {
-    'williamboman/mason.nvim',
-    cmd = 'Mason',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('mason').setup()
-      require('mason-lspconfig').setup()
-      require('mason-lspconfig').setup_handlers({
-        function(server_name)
-          local capabilities = require('cmp_nvim_lsp').default_capabilities()
-          require('lspconfig')[server_name].setup({
-            capabilities = capabilities
-          })
-        end,
-        ['clangd'] = function()
-          local capabilities = require('cmp_nvim_lsp').default_capabilities()
-          require('lspconfig').clangd.setup({
-            capabilities = capabilities,
-            cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
-            init_options = {
-              fallbackFlags = { '-std=c++17' },
-            },
-          })
-        end,
-        ['lua_ls'] = function()
-          local capabilities = require('cmp_nvim_lsp').default_capabilities()
-          require 'lspconfig'.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT',
-                },
-                diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = { 'vim', 'MiniPick', 'MiniBufremove', 'MiniExtras' },
-                },
-                workspace = {
-                  -- Make the server aware of Neovim runtime files
-                  library = vim.api.nvim_get_runtime_file("./*.lua", true),
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                  enable = false,
-                },
-              },
-            },
-          }
-        end,
-    })
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = {
-        'lua_ls',
-        -- 'jdtls'
-      },
-      automatic_installation = true
-    },
-  },
+  -- {
+  --   'williamboman/mason.nvim',
+  --   cmd = 'Mason',
+  --   event = { 'BufReadPre', 'BufNewFile' },
+  --   config = function()
+  --     require('mason').setup()
+  --     require('mason-lspconfig').setup()
+  --     require('mason-lspconfig').setup_handlers({
+  --       function(server_name)
+  --         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  --         require('lspconfig')[server_name].setup({
+  --           capabilities = capabilities
+  --         })
+  --       end,
+  --       ['clangd'] = function()
+  --         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  --         require('lspconfig').clangd.setup({
+  --           capabilities = capabilities,
+  --           cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+  --           init_options = {
+  --             fallbackFlags = { '-std=c++17' },
+  --           },
+  --         })
+  --       end,
+  --       ['lua_ls'] = function()
+  --         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  --         require 'lspconfig'.lua_ls.setup {
+  --           capabilities = capabilities,
+  --           settings = {
+  --             Lua = {
+  --               runtime = {
+  --                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+  --                 version = 'LuaJIT',
+  --               },
+  --               diagnostics = {
+  --                 -- Get the language server to recognize the `vim` global
+  --                 globals = { 'vim', 'MiniPick', 'MiniBufremove', 'MiniExtras' },
+  --               },
+  --               workspace = {
+  --                 -- Make the server aware of Neovim runtime files
+  --                 library = vim.api.nvim_get_runtime_file("./*.lua", true),
+  --               },
+  --               -- Do not send telemetry data containing a randomized but unique identifier
+  --               telemetry = {
+  --                 enable = false,
+  --               },
+  --             },
+  --           },
+  --         }
+  --       end,
+  --   })
+  --   end
+  -- },
+  -- {
+  --   'williamboman/mason-lspconfig.nvim',
+  --   opts = {
+  --     ensure_installed = {
+  --       'lua_ls',
+  --       -- 'jdtls'
+  --     },
+  --     automatic_installation = true
+  --   },
+  -- },
   {
     'neovim/nvim-lspconfig',
     init = function()
