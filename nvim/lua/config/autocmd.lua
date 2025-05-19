2d61d244c6eb95561d690b727cd7bc10bc7edfe3
Mike Medford <mike@medford.dev>
2025-05-19 15:57:36 -0500

    avoid running on class files

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,12 +1,12 @@
 -- Save folds
 vim.api.nvim_create_autocmd({"BufWinLeave"}, {
-  pattern = {"*.*"},
+  pattern = {"!*.class"},
   desc = "save view (folds), when closing file",
   command = "mkview",
 })
 -- Load folds
 vim.api.nvim_create_autocmd({"BufWinEnter"}, {
-  pattern = {"*.*"},
+  pattern = {"!*.class"},
   desc = "load view (folds), when opening file",
   command = "silent! loadview"
 })
